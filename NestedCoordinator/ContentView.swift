5acc791c480a5db5e3b9780a19d722d3aa651de3
maopos <mauricioposadamurillo@gmail.com>
2025-02-02 16:11:58 -0500

    Initial commit

---
+++ NestedCoordinator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NestedCoordinator
+//
+//  Created by Mauricio Posada Murillo on 2/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NestedCoordinator/NestedCoordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NestedCoordinatorApp.swift
+//  NestedCoordinator
+//
+//  Created by Mauricio Posada Murillo on 2/02/25.
+//
+
+import SwiftUI
+
+@main
+struct NestedCoordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
